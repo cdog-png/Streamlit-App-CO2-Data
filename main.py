770b6020a8a6da016f50ae3a700eb7a8196e1cea
cdog-png <134445015+cdog-png@users.noreply.github.com>
2023-05-25 21:25:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ columns = ['co2_per_gdp', 'oil_co2_per_capita', 'cement_co2_per_capita', 'gas_co
            'co2_including_luc_per_capita', 'co2_per_capita']
 
 fig = px.scatter_matrix(df_normalized[columns], title="Pairplot", dimensions=columns)
-fig.update_layout(width=900, height=700)  # Adjust the width and height as per your preference
+fig.update_layout(width=900, height=900)  # Adjust the width and height as per your preference
 
 st.plotly_chart(fig)
 
