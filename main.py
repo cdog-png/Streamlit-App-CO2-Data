d5b25beae8c9e31194db5138823f84e9eadd5a1c
cdog-png <134445015+cdog-png@users.noreply.github.com>
2023-05-25 14:24:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,3 +44,123 @@ top_20_countries = grouped_data.nlargest(20)
 
 # Create a Streamlit bar chart for top countries
 st.bar_chart(top_20_countries, use_container_width=True)
+
+# Step 11: Create GDP per capita variable
+df['gdp_per_capita'] = df['gdp'] / df['population']
+
+# Step 12: Plot relationship between GDP per capita and CO2 per unit of energy
+fig = plt.figure(figsize=(8, 6))
+sns.regplot(x=df_2018['gdp_per_capita'], y=df_2018['co2_per_unit_energy'], color='blue')
+plt.xlabel('GDP per capita')
+plt.ylabel('CO2 per unit energy')
+plt.title('Relationship between GDP per capita and CO2 per unit of energy (2018)')
+st.pyplot(fig)
+
+# Step 13: Print unique countries and create continental lists
+unique_countries = df['country'].unique()
+continents = {
+    'africa': ['Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cameroon', 'Cape Verde',
+               'Central African Republic', 'Chad', 'Comoros', 'Congo', 'Democratic Republic of Congo', 'Djibouti',
+               'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea',
+               'Guinea-Bissau', 'Ivory Coast', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali',
+               'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda',
+               'Sao Tome and Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa',
+               'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe'],
+    'asia': ['Afghanistan', 'Armenia', 'Azerbaijan', 'Bahrain', 'Bangladesh', 'Bhutan', 'Brunei', 'Cambodia', 'China',
+             'Cyprus', 'Georgia', 'India', 'Indonesia', 'Iran', 'Iraq', 'Israel', 'Japan', 'Jordan', 'Kazakhstan',
+             'Kuwait', 'Kyrgyzstan', 'Laos', 'Lebanon', 'Malaysia', 'Maldives', 'Mongolia', 'Myanmar', 'Nepal',
+             'North Korea', 'Oman', 'Pakistan', 'Palestine', 'Philippines', 'Qatar', 'Russia', 'Saudi Arabia',
+             'Singapore', 'South Korea', 'Sri Lanka', 'Syria', 'Taiwan', 'Tajikistan', 'Thailand', 'Timor-Leste',
+             'Turkey', 'Turkmenistan', 'United Arab Emirates', 'Uzbekistan', 'Vietnam', 'Yemen'],
+    'europe': ['Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia',
+               'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
+               'Iceland', 'Ireland', 'Italy', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta',
+               'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'North Macedonia', 'Norway', 'Poland', 'Portugal',
+               'Romania', 'Russia', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland',
+               'Ukraine', 'United Kingdom', 'Vatican City'],
+    'north_america': ['Canada', 'Mexico', 'United States'],
+    'south_america': ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Paraguay', 'Peru',
+                      'Suriname', 'Uruguay', 'Venezuela'],
+    'oceania': ['Australia', 'Fiji', 'Kiribati', 'Marshall Islands', 'Micronesia', 'Nauru', 'New Zealand', 'Palau',
+                'Papua New Guinea', 'Samoa', 'Solomon Islands', 'Tonga', 'Tuvalu', 'Vanuatu']
+}
+
+# Step 14: Create new continent column (2018)
+df_2018['continent'] = df_2018['country'].apply(
+    lambda x: next((continent for continent, countries in continents.items() if x in countries), 'Unknown')
+)
+
+# Display the modified DataFrame
+st.write("DataFrame for 2018 with continent column:")
+st.write(df_2018)
+
+# Step 15: Compute mean share of global oil CO2 emissions by continent (2018)
+co2_by_continent = df_2018.groupby('continent')['share_global_oil_co2'].mean()
+
+# Create bar plot
+fig = plt.figure(figsize=(8, 6))
+co2_by_continent.plot(kind='bar', color='blue')
+plt.xlabel('Continent')
+plt.ylabel('Share of global oil CO2 emissions')
+plt.title('Mean share of global oil CO2 emissions by continent in 2018')
+st.image(fig)
+
+# Step 16: Replicate graph for 1990
+mask = df['year'] == 1990
+df_1990 = df[mask]
+
+# Create new continent column
+continent = []
+for country in df_1990['country']:
+    if country in asia:
+        continent.append('Asia')
+    elif country in europe:
+        continent.append('Europe')
+    elif country in north_america:
+        continent.append('North America')
+    elif country in south_america:
+        continent.append('South America')
+    elif country in africa:
+        continent.append('Africa')
+    else:
+        continent.append('Oceania')
+df_1990['continent'] = continent
+
+# Compute mean share of global oil CO2 emissions by continent
+co2_by_continent = df_1990.groupby('continent')['share_global_oil_co2'].mean()
+
+# Create bar plot
+fig = plt.figure(figsize=(8, 6))
+co2_by_continent.plot(kind='bar', color='blue')
+plt.xlabel('Continent')
+plt.ylabel('Share of global oil CO2 emissions')
+plt.title('Mean share of global oil CO2 emissions by continent in 1990')
+st.image(fig)
+
+# Step 17: Plot top countries by CO2 emissions
+grouped_data = df_1990.groupby('country')['co2_per_capita'].sum()
+top_20_countries = grouped_data.nlargest(20)
+
+fig = plt.figure(figsize=(15, 8))
+plt.bar(top_20_countries.index, top_20_countries.values, width=0.5)
+plt.xlabel('Country')
+plt.ylabel('Per capita CO2 emissions in 1990')
+plt.title('Per capita CO2 emissions by country in 1990')
+plt.xticks(rotation=50, fontsize=10)
+st.image(fig)
+
+# Step 18: Show relationship between CO2 per capita and GDP per capita in 1990
+fig = plt.figure(figsize=(8, 6))
+sns.regplot(x=df_1990['gdp_per_capita'], y=df_1990['co2_per_capita'], color='blue')
+plt.xlabel('GDP per capita')
+plt.ylabel('CO2 per capita')
+plt.title('Relationship between GDP per capita and CO2 per capita (1990)')
+st.image(fig)
+
+# Step 19: Show relationship between CO2 per capita and GDP per capita in 2018
+fig = plt.figure(figsize=(8, 6))
+sns.regplot(x=df_2018['gdp_per_capita'], y=df_2018['co2_per_capita'], color='blue')
+plt.xlabel('GDP per capita')
+plt.ylabel('CO2 per capita')
+plt.title('Relationship between GDP per capita and CO2 per capita (2018)')
+st.image(fig)
